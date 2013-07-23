d27464d07183de6d11644a496a795c5d28a37b67
David Lau <bbc093@hotmail.com>
2013-07-23 16:53:07 -0400

    added carousal to places page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
-// $.ui.ready(
-//     function(){
-//         $(".featured_pg").toggleHeaderMenu(false); 
-//     }
-// );
 
-
-// $("li").addClass("foo");
-// $.ui.clearHistory("featured_pg");
-
-// $.ui.removeFooterMenu("#main");
-// $.ui.setBackButtonText("Go Back");
-
-$(".place_unit").swipeDown(function(){
+$("#events_car").swipeUp(function(){
     $(this).css3Animate({        
-        y: "265px",
+        y: "-200px",
         timingFunction: "ease",
-        time: "1000ms",
+        time: "200ms",
     });
-}).swipeUp(function(){
+}).swipeDown(function(){
 	$(this).css3Animate({        
 		y: "0",
         timingFunction: "ease",
-        time: "1000ms",
+        time: "200ms",
     });
 });
+
+// Carousel
+
+var options={
+   vertical:false, // page up/down
+   horizontal:true, // page left/right
+   pagingDiv:null, // div to hold the dots for paging
+   pagingCssName:"carousel_paging", //classname for the paging dots
+   pagingCssNameSelected: "carousel_paging_selected", //classname for the selected page dots
+   wrap:true //Creates a continuous carousel
+}
+
+var carousel = $("#events_car").carousel();
